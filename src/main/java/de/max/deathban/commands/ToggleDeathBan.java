d2d1e739f4ca864f1442127f61332a1f39e05e27
ItsLeMax <lemaxyt1243@gmail.com>
2025-01-07 07:04:24 +0100

    ILMLib update and feature usage, merges, plugin name hardcoded

--- src/main/java/de/max/deathban/commands/ToggleDeathBan.java
+++ src/main/java/de/max/deathban/commands/ToggleDeathBan.java
@@ -1,23 +1,26 @@
 package de.max.deathban.commands;
 
 import de.max.deathban.init.Methods;
+import de.max.ilmlib.libraries.MessageLib;
+import de.max.ilmlib.utility.HoverText;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
 import static de.max.deathban.init.DeathBan.configLib;
+import static de.max.deathban.init.DeathBan.messageLib;
 
 public class ToggleDeathBan implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (args.length > 0) {
-            sender.sendMessage("§c" + configLib.lang("commands.tooManyArgs"));
+            messageLib.sendInfo(sender, MessageLib.Template.ERROR, configLib.lang("commands.tooManyArgs"), new HoverText("/toggledeathban"));
             return true;
         }
 
         if (!sender.hasPermission("deathban.toggle")) {
-            sender.sendMessage("§c" + configLib.lang("general.noPerms"));
+            messageLib.sendInfo(sender, MessageLib.Template.ERROR, configLib.lang("general.noPerms"));
             return true;
         }
 
--- src/main/java/de/max/deathban/init/DeathBan.java
+++ src/main/java/de/max/deathban/init/DeathBan.java
@@ -3,30 +3,39 @@ package de.max.deathban.init;
 import de.max.deathban.commands.ToggleDeathBan;
 import de.max.deathban.events.PlayerDeath;
 import de.max.deathban.events.PlayerJoin;
-import de.max.ilmlib.init.ILMLib;
 import de.max.ilmlib.libraries.ConfigLib;
+import de.max.ilmlib.libraries.MessageLib;
+import de.max.ilmlib.utility.ErrorTemplate;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DeathBan extends JavaPlugin {
+
     public static DeathBan plugin;
     public static ConfigLib configLib;
+    public static MessageLib messageLib;
 
     @Override
     @SuppressWarnings("all")
     public void onEnable() {
         plugin = this;
-
-        configLib = new ILMLib(plugin).getConfigLib();
-        configLib
+        configLib = new ConfigLib()
+                .setPlugin(this)
                 .createDefaults("config")
                 .createInsideDirectory("languages", "de_DE", "en_US", "custom_lang");
+
+        messageLib = new MessageLib()
+                .addSpacing()
+                .createDefaults()
+                .setPrefix("§6DeathBan §7»", true);
+
+        new ErrorTemplate().setSuffix(configLib.lang("general.error"));
 
         Bukkit.getPluginManager().registerEvents(new PlayerJoin(), plugin);
         Bukkit.getPluginManager().registerEvents(new PlayerDeath(), plugin);
 
         getCommand("toggledeathban").setExecutor(new ToggleDeathBan());
 
-        Bukkit.getConsoleSender().sendMessage("§6" + configLib.lang("general.init"));
+        Bukkit.getConsoleSender().sendMessage("§6" + configLib.lang("general.init").replace("%p%", "[DeathBan]"));
     }
 }
